b6ffdb2a7603be1263ae7e57fd6fcabb401b78d4
VAIBHAVI R. BHURANE <117935143+vaibhavibhurane@users.noreply.github.com>
2023-07-31 21:35:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let menuBtn = document.querySelector('#menu-btn'); 
+let menuBar = document.querySelector('.menu-bar');
+
+menuBtn.onclick = () => {
+
+    menuBtn.classList.toggle('bx-x');
+    menuBar.classList.toggle('active');
+
+}
+window.onscroll = () => {
+
+    menuBtn.classList.remove('bx-x');
+    menuBar.classList.remove('active');
+}
+
+ScrollReveal({
+    reset: true,
+    distance: '100px',
+    duration: 2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-bio h1', { origin:'left'});
+ScrollReveal().reveal('.home-bio p', { origin: 'right'});
